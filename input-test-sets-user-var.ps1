bed5876fa14e1fb5f2b55266643b909ff128626c
juanpickselov <juanpickselov@sbcglobal.net>
2021-11-07 15:24:57 -0500

    moved other variables out of the secret for loop

--- input-test-sets-user-var.ps1
+++
@@ -1,5 +0,0 @@
-$secureScrt = Read-Host "Enter phrase" -AsSecureString
-
-$plainPwd =[Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureScrt))
-
-[Environment]::SetEnvironmentVariable("USER_PHRASE", $plainPwd, "User")
